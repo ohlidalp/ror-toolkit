6fa2f53d03399fb1b657d70eedd1ef7c968730fd
rorthomas <rorthomas@b496f173-4f29-0410-80f1-b965c90768b3>
2007-06-29 08:29:01 +0000

    * deleted old settings class * added new settingsManager and logger * added update bat script * added config files for logging and editor.ini * modified existing classes to use new settingsManager and logger - not completed

---
+++ lib/ror/logger.py
@@ -0,0 +1,22 @@
+import os, os.path, sys, logging,logging.config
+
+LOGCONFIGFILE = "logging.ini"
+
+# "extended" singleton
+_rorlogger = None
+def log():
+    global _rorlogger
+    if _rorlogger is None:
+        _rorlogger = RoRLogger()
+    return _rorlogger.getLog()
+            
+class RoRLogger():
+    logconfigfilename = os.path.join(os.path.dirname(os.path.abspath(__file__)), LOGCONFIGFILE)
+
+    def __init__(self):
+        logging.config.fileConfig(self.logconfigfilename)
+        self.myLog = logging.getLogger("root")
+        self.myLog.info("logging initialised")
+
+    def getLog(self):
+        return self.myLog
