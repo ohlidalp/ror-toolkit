838f4e390dfd523b7ba51cfc66bfc3c941ae9349
rorthomas <rorthomas@b496f173-4f29-0410-80f1-b965c90768b3>
2007-07-08 19:13:57 +0000

    * added missing file :-\

---
+++ trunk/lib/ror/depcheckerplugins/deptools.py
@@ -0,0 +1,12 @@
+#Thomas Fischer 06/07/2007, thomas@thomasfischer.biz
+import sys, os, os.path
+
+REQUIRES = 'requires'
+OPTIONAL = 'optional'
+PROVIDES = 'provides'
+REQUIREDBY = 'requiredby'
+RELATIONS = [REQUIRES, PROVIDES, OPTIONAL, REQUIREDBY]
+
+MATERIAL = 'material'
+FILE = 'file'
+TYPES = [MATERIAL, FILE]
