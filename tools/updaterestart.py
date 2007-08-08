ab57e24a8b12ac209018e22a08523e11350ac59c
rorthomas <rorthomas@b496f173-4f29-0410-80f1-b965c90768b3>
2007-08-08 01:37:42 +0000

    * bugfixed automatic restart after update

--- tools/updaterestart.py
+++ tools/updaterestart.py
@@ -4,8 +4,8 @@ from subprocess import Popen
 def main():
     import time
     time.sleep(1)
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rortoolkit.bat")
-    p = Popen(path, shell = True)
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "rortoolkit.py")
+    p = Popen(path, shell = True, cwd = os.path.dirname(path))
     sys.exit(0)
 
 if __name__=="__main__": 
