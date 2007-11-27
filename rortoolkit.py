6e49957324a70c14ee6600d643d268fd4356c2c9
rorthomas <rorthomas@b496f173-4f29-0410-80f1-b965c90768b3>
2007-11-27 15:31:10 +0000

    updated setup, removed menu

--- rortoolkit.py
+++ rortoolkit.py
@@ -6,24 +6,30 @@ def main():
 	"""
 	main method
 	"""
+	rorexecutable = ''
 	if sys.platform in ['linux', 'linux2']:
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_common"))
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_linux"))
+		rorexecutable = "RoR.bin"
 	elif sys.platform in ['win32']:
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_common"))
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_windows"))
+		rorexecutable = "RoR.exe"
+
+	import ror.rorcommon
+	if not ror.rorcommon.checkRoRDirectory():
+		import ror.starter
+		ror.starter.startApp()
 
 	# Import Psyco if available
 	try:
 		import psyco
-		#psyco.full()
-		#psyco.log()
-		#psyco.profile()
+		psyco.full()
 	except ImportError:
 		pass
 
-	import ror.starter
-	ror.starter.startApp()
+	import roreditor.MainFrame
+	roreditor.MainFrame.startApp()
 
 
 if __name__=="__main__":
--- rortoolkit_old.py
+++ rortoolkit_old.py
@@ -6,30 +6,24 @@ def main():
 	"""
 	main method
 	"""
-	rorexecutable = ''
 	if sys.platform in ['linux', 'linux2']:
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_common"))
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_linux"))
-		rorexecutable = "RoR.bin"
 	elif sys.platform in ['win32']:
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_common"))
 		sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib_windows"))
-		rorexecutable = "RoR.exe"
-
-	import ror.rorcommon
-	if not ror.rorcommon.checkRoRDirectory():
-		import ror.starter
-		ror.starter.startApp()
 
 	# Import Psyco if available
 	try:
 		import psyco
-		psyco.full()
+		#psyco.full()
+		#psyco.log()
+		#psyco.profile()
 	except ImportError:
 		pass
 
-	import roreditor.MainFrame
-	roreditor.MainFrame.startApp()
+	import ror.starter
+	ror.starter.startApp()
 
 
 if __name__=="__main__":
